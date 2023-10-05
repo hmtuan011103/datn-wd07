5fe1ca5633013bb5cb24a2fc5783b004788f9981
hmtuan011103 <hoangmanhtuan0111@gmail.com>
2023-10-05 14:26:04 +0700

    change name admin to manage to route admin

--- routes/admin.php
+++ routes/admin.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Admin Routes ( With default prefix admin and url is: http://127.0.0.1:8000/admin + route )
+| Admin Routes ( With default prefix admin and url is: http://127.0.0.1:8000/manage + route )
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
