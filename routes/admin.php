6f7827023dafb295ee8d7cca85073579974dd62a
tham1412 <thamnhph28710@fpt.edu.vn>
2023-10-07 22:44:36 +0700

    permission v1

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Locations\Admin\LocationController;
+use App\Http\Controllers\PermissionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,5 +18,12 @@ Route::get('/', function () {
     return view('admin.pages.home.index', [
         'title' => 'Quản trị chiến thắng'
     ]);
+});
+Route::prefix('permission')->group(function () {
+    Route::get('/', [PermissionController::class, 'list'])->name('listPermission');
+    Route::post('save_permissions',[PermissionController::class,'add'])->name('addPermission');
+    Route::get('edit/{id}',[PermissionController::class,'edit'])->name('editPermission');
+    Route::post('save_edit/{id}',[PermissionController::class,'save_edit'])->name('saveEditPermission');
+    Route::get('delete/{id}',[PermissionController::class,'delete'])->name('deletePermission');
 });
 
