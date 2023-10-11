f7d4a4b56af7b05a004ebfbc01096ad6011cc9d4
maohiemgia <maohiemgia@gmail.com>
2023-10-11 21:27:44 +0700

    upload jquery

--- resources/views/admin/pages/user/script.blade.php
+++ resources/views/admin/pages/user/script.blade.php
@@ -1,5 +1,5 @@
 <!--jquery cdn-->
-<script src={{ asset('jquery-3.6.0.min.js') }}></script>
+<script src={{ asset('admin/assets/libs/jquery/jquery-3.6.0.min.js') }}></script>
 <!--sweetalert-->
 <script src={{ asset('admin/assets/libs/sweetalert2/sweetalert2.min.js') }}></script>
 
