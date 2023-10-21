6ccac053f30ce195cf085bba8dd9a34952e9972e
DoVanNam28617 <namdvph28617@fpt.edu.vn>
2023-10-21 14:09:07 +0700

    search_trip

--- app/Http/Controllers/Trip/Client/TripController.php
+++ app/Http/Controllers/Trip/Client/TripController.php
@@ -5,9 +5,27 @@ namespace App\Http\Controllers\Trip\Client;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Trip\BaseTripController;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class TripController extends BaseTripController
 {
     //
+    public function searchTrip(Request $request)
+    {
+        if ($request->type_ticket == 1) {
+            $trips = $this->tripService->searchTrip($request);
+            if ($trips == null) {
+                return response()->json(['status' => 0,$trips], 200);
+            } else {
+                return response()->json(['status' => 1,$trips], 200);
+            }
+        } else {
+            $trips = $this->tripService->searchTrip($request);
+            if ($trips == null) {
+                return response()->json(['status' => 0,$trips], 200);
+            } else {
+                return response()->json(['status' => 2,$trips], 200);
+            }
+            
+        }
+    }
 }
--- app/Services/Trips/TripService.php
+++ app/Services/Trips/TripService.php
@@ -6,10 +6,13 @@ use App\Http\Requests\Trip\StoreTripRequest;
 use App\Http\Requests\Trip\UpdateTripRequest;
 use App\Models\Bills;
 use App\Models\Car;
-use App\Models\Location;
 use App\Models\Trip;
 use App\Models\User;
 use Carbon\Carbon;
+use DateInterval;
+use DateTime;
+
+use function PHPUnit\Framework\isEmpty;
 
 class TripService
 {
@@ -20,47 +23,30 @@ class TripService
     public function list_desc()
     {
         // $trips = Trip::all();       
-        $trips = Trip::select('trips.id', 'trips.assistantCar_id', 'trips.car_id', 'trips.drive_id', 'trips.start_date', 'trips.start_time', 'trips.start_location', 'trips.status', 'trips.trip_price', 'trips.end_location', 'trips.created_at', 'trips.updated_at', 'cars.name as car_name', 'users.name as user_name')
-            ->join('cars', 'cars.id', '=', 'trips.car_id')
-            ->join('users', 'users.id', '=', 'trips.drive_id')
-            ->orderBy('updated_at', 'DESC')->get();
+        $trips = Trip::select('trips.id', 'tríp.assistantCar_id','trips.car_id', 'trips.drive_id','trips.start_date','trips.start_time','trips.start_location','trips.status','trips.trip_price','trips.end_location','trips.created_at','trips.updated_at','cars.name as car_name','users.name as user_name')
+        ->join('cars', 'cars.id', '=', 'trips.car_id')
+        ->join('users', 'users.id', '=', 'trips.drive_id')
+        ->orderBy('updated_at', 'DESC')->get();
         return $trips;
     }
 
-    public function create (StoreTripRequest $request) {
-        if($request->isMethod('POST')) {
-          
+    public function create(StoreTripRequest $request)
+    {
+        if ($request->isMethod('POST')) {
+
             $params = $request->all();
-            // dd($params);
-            $timeFloat = $request->interval_trip;
-            $hourMinute = str_replace(['giờ', 'phút'], '', $timeFloat);
-            $hourMinuteArray = explode(' ', $hourMinute);
-            $hour = $hourMinuteArray[0];
-            $minute = $hourMinuteArray[1];
-
-            // Định dạng thành chuỗi "H:i:s"
-            $time = sprintf("%02d:%02d:00", $hour, $minute);
-
-            $params['interval_trip'] = $time;
-            // dd($params);
             unset($params['_token']);
 
-            return Trip::create($params);   
-
-
-
+            return Trip::create($params);
         }
     }
-    public function get_parent_id()
+
+
+    public function edit_trip(StoreTripRequest $request, $id)
     {
-        return Location::select('locations.name', 'locations.id')->where('locations.parent_id', Null)->get();
-    }
-
-
-    public function edit_trip(StoreTripRequest $request , $id) {
         Trip::find($id);
-        if($request->isMethod('POST')) {
-            $params = $request->except('proengsoft_jsvalidation','_token');
+        if ($request->isMethod('POST')) {
+            $params = $request->except('proengsoft_jsvalidation', '_token');
             // dd($params);
             return Trip::where('id', $id)->update($params);
         }
@@ -70,6 +56,93 @@ class TripService
     {
         Trip::find($id)->delete();
         return redirect()->route('list_trip');
+    }
 
+    public function searchTrip($request)
+    {
+        $currentDate = date('Y/m/d');
+        $currentTime = date('H:i:s');
+        $currenttime = new DateTime($currentTime);
+        $interval = new DateInterval('PT4H00M');
+        $currenttime->add($interval);
+        $currtime = $currenttime->format('H:i:s');
+        if ($request->type_ticket == 1) {
+
+            $trips = Trip::with('car.typeCar')->where(['start_location' => $request->start_location, 'end_location' => $request->end_location, 'start_date' => $request->start_date, 'status' =>'1'])->orderBy('start_time')->get();
+            $total_trip = count($trips);
+            for ($i = 0; $i < $total_trip; $i++) {
+                if ($currentDate == $request->start_date) {
+                    if ($trips[$i]->start_time < $currtime) {
+                        unset($trips[$i]);
+                        continue;
+                    }
+                }
+                $total_seat_trip = $trips[$i]->car->typeCar->total_seat;
+                $seat_bill = Bills::select('total_seats')->where(['trip_id' => $trips[$i]->id])->get();
+                $total_seat_bill = 0;
+                foreach ($seat_bill as $value) {
+                    $total_seat_bill += intval($value->total_seats);
+                }
+                $seats = intval($total_seat_trip) - intval($total_seat_bill);
+                if ($seats < intval($request->ticket)) {
+                    unset($trips[$i]);
+                }
+            }
+
+            if ($trips->isEmpty()) {
+                return null;
+            } else {
+                return $trips;
+            }
+        } else {
+
+            $tripstart = Trip::with('car.typeCar')->where(['start_location' => $request->start_location, 'end_location' => $request->end_location, 'start_date' => $request->start_date, 'status' =>'1'])->orderBy('start_time')->get();
+            $tripend = Trip::with('car.typeCar')->where(['start_location' => $request->end_location, 'end_location' => $request->start_location, 'start_date' => $request->end_date, 'status' =>'1'])->orderBy('start_time')->get();
+            $total_trip_start = count($tripstart);
+            for ($i = 0; $i < $total_trip_start; $i++) {
+                if ($currentDate == $request->start_date) {
+                    if ($tripstart[$i]->start_time < $currtime) {
+                        unset($tripstart[$i]);
+                        continue;
+                    }
+                }
+                $total_seat_trip = $tripstart[$i]->car->typeCar->total_seat;
+                $seat_bill = Bills::select('total_seats')->where(['trip_id' => $tripstart[$i]->id])->get();
+                $total_seat_bill = 0;
+                foreach ($seat_bill as $value) {
+                    $total_seat_bill += intval($value->total_seats);
+                }
+                $seats = intval($total_seat_trip) - intval($total_seat_bill);
+                if ($seats < intval($request->ticket)) {
+                    unset($tripstart[$i]);
+                }
+            }
+            $total_trip_end = count($tripend);
+            for ($i = 0; $i < $total_trip_end; $i++) {
+                if ($currentDate == $request->start_date) {
+                    if ($tripend[$i]->start_time < $currtime) {
+                        unset($tripend[$i]);
+                        continue;
+                    }
+                }
+                $total_seat_trip = $tripend[$i]->car->typeCar->total_seat;
+                $seat_bill = Bills::select('total_seats')->where(['trip_id' => $tripend[$i]->id])->get();
+                $total_seat_bill = 0;
+                foreach ($seat_bill as $value) {
+                    $total_seat_bill += intval($value->total_seats);
+                }
+
+                $seats = intval($total_seat_trip) - intval($total_seat_bill);
+
+                if ($seats < intval($request->ticket)) {
+                    unset($tripend[$i]);
+                }
+            }
+            if ($tripstart->isEmpty() || $tripend->isEmpty()) {
+                return null;
+            } else {
+                return [$tripstart, $tripend];
+            }
+        }
     }
 }
