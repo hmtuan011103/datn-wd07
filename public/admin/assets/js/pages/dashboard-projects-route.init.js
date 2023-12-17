b71cc68b338e3e4bf98fe9f3d4e827dadc3e56da
synt <synt1403@gmail.com>
2023-12-17 22:16:20 +0700

    up

--- public/admin/assets/js/pages/dashboard-projects-route.init.js
+++ public/admin/assets/js/pages/dashboard-projects-route.init.js
@@ -154,73 +154,7 @@ function dataChart(data) {
                 ))),
                 {});
     // chart.updateSeries(data);
+    console.log(chart)
     chart.render()
-    // dataChart(data)
-    // isApexSeries = document.querySelectorAll("[data-chart-series]"),
-    // donutchartProjectsStatusColors =
-    //   (isApexSeries &&
-    //     Array.from(isApexSeries).forEach(function (e) {
-    //       var t,
-    //         e = e.attributes;
-    //       e["data-chart-series"] &&
-    //         ((isApexSeriesData.series = e["data-chart-series"].value.toString()),
-    //           (t = getChartColorsArray(e.id.value.toString())),
-    //           (t = {
-    //             series: [isApexSeriesData.series],
-    //             chart: {
-    //               type: "radialBar",
-    //               width: 36,
-    //               height: 36,
-    //               sparkline: { enabled: !0 },
-    //             },
-    //             dataLabels: { enabled: !1 },
-    //             plotOptions: {
-    //               radialBar: {
-    //                 hollow: { margin: 0, size: "50%" },
-    //                 track: { margin: 1 },
-    //                 dataLabels: { show: !1 },
-    //               },
-    //             },
-    //             colors: t,
-    //           }),
-    //           new ApexCharts(
-    //             document.querySelector("#" + e.id.value.toString()),
-    //             t
-    //           ).render());
-    //     }),
-    //     getChartColorsArray("prjects-status"))
-    //     ,
-    // currentChatId =
-    //   (donutchartProjectsStatusColors &&
-    //     ((options = {
-    //       series: [125, 42, 58, 89],
-    //       labels: ["Completed", "In Progress", "Yet to Start", "Cancelled"],
-    //       chart: { type: "donut", height: 230 },
-    //       plotOptions: {
-    //         pie: {
-    //           size: 100,
-    //           offsetX: 0,
-    //           offsetY: 0,
-    //           donut: { size: "90%", labels: { show: !1 } },
-    //         },
-    //       },
-    //       dataLabels: { enabled: !1 },
-    //       legend: { show: !1 },
-    //       stroke: { lineCap: "round", width: 0 },
-    //       colors: donutchartProjectsStatusColors,
-    //     }),
-    //       (chart = new ApexCharts(
-    //         document.querySelector("#prjects-status"),
-    //         options
-    //       )).render()),
-    //     "users-chat");
-    // function scrollToBottom(e) {
-    //   setTimeout(() => {
-    //     new SimpleBar(
-    //       document.getElementById("chat-conversation")
-    //     ).getScrollElement().scrollTop =
-    //       document.getElementById("users-conversation").scrollHeight;
-    //   }, 100);
-    // }
-    // scrollToBottom(currentChatId);
+    
 }
